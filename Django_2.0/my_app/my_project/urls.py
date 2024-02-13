fe45fc8fe22f7c30bc54b4c4ab3448f2bd342e59
marcel <marcel.petzer@4-sure.net>
2024-02-13 13:56:56 +0200

    added main page along with all functions needed

--- Django_2.0/my_app/my_project/urls.py
+++ Django_2.0/my_app/my_project/urls.py
@@ -6,12 +6,12 @@ from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 router.register(r'drones', views.DroneModelViewSet)
 router.register(r'medications', views.MedicationViewSet)
-
-router.register(r'dronemedication', views.DroneMedicationViewSet)
+router.register(r'Load/drone', views.DroneMedicationViewSet)
 router.register(r'batterylogs', views.BatteryLogViewSet)
 urlpatterns = [
     path('Capture-Information/', include(router.urls)),
-    path('batteryinfo/drones_with_low_battery/',views.drones_with_low_battery),
-    path('batteryinfo/drones_battery/',views.drone_battery)
+    path('battery_info/drones_with_low_battery/',views.drones_with_low_battery),
+    path('battery_info/drones_battery/',views.drone_battery)
+    
 ]
 
